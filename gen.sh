1470df08d5db488ccaf687e5d1d74fe159e6d72e
luoxianzun <b00txt@163.com>
2025-02-16 23:41:45 +0800

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-wget -O geneva.py http://d.16idc.com/shell/geneva.py
+wget -O gen.py https://raw.githubusercontent.com/luoxianzun/shell/refs/heads/main/gen.py
 yum install -y python3 python3-devel gcc gcc-c++ git libnetfilter* libffi-devel
 pip3 install --upgrade pip
 pip3 install scapy netfilterqueue
-nohup python3 geneva.py -q 100 -w 17 &
-nohup python3 geneva.py -q 101 -w 4 &
-iptables -I OUTPUT -p tcp --sport 80 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 100
-iptables -I OUTPUT -p tcp --sport 443 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 101
-echo "geneva.py install finish, please verify it with [ps -ef|grep geneva]"
+nohup python3 gen.py -q 100 -w 1 -s 7 -c 0 -n 7 &
+iptables -I OUTPUT -p tcp --sport 443 -j NFQUEUE --queue-num 100
+iptables -I OUTPUT -p tcp --sport 80 -j NFQUEUE --queue-num 100
+echo "gen.py install finish, please verify it with [ps -ef|grep gen]"
