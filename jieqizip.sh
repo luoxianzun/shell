bf61f6552c03f31e3cbec479813d4d5b73d7e7a1
luoxianzun <85412464+luoxianzun@users.noreply.github.com>
2025-02-22 03:49:46 +0800

    Update jieqizip.sh

--- jieqizip.sh
+++ jieqizip.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 检查是否传入了网站名称参数
+if [ -z "$1" ]; then
+  echo "Error: No site name provided."
+  exit 1
+fi
+
 # 设置网站名称（从脚本参数获取）
 SITE_NAME=$1
 
@@ -7,7 +13,7 @@ SITE_NAME=$1
 ARCHIVE_NAME="${SITE_NAME}.zip"
 
 # 输出目录
-DIR_OUT="/www/wwwroot/site/"
+DIR_OUT="/www/wwwroot/site"
 
 # 检查并创建输出目录结构
 if [ ! -d "$DIR_OUT" ]; then
@@ -18,8 +24,8 @@ else
 fi
 
 # 设置要压缩的目录和排除的目录
-DIRECTORY_TO_ZIP="/www/wwwroot/$SITE_NAME/"
-EXCLUDE_DIRECTORY="files"
+DIRECTORY_TO_ZIP="/www/wwwroot/$SITE_NAME"
+EXCLUDE_DIRECTORY="$DIRECTORY_TO_ZIP/files/*"
 
 # 检查要压缩的目录是否存在
 if [ ! -d "$DIRECTORY_TO_ZIP" ]; then
@@ -28,7 +34,7 @@ if [ ! -d "$DIRECTORY_TO_ZIP" ]; then
 fi
 
 # 压缩网站根目录文件，排除files目录
-zip -r "$DIR_OUT$ARCHIVE_NAME" "$DIRECTORY_TO_ZIP" -x "$DIRECTORY_TO_ZIP/$EXCLUDE_DIRECTORY/*"
+zip -r "$DIR_OUT/$ARCHIVE_NAME" "$DIRECTORY_TO_ZIP" -x "$EXCLUDE_DIRECTORY"
 
 # 压缩image目录（如果存在）
 IMAGE_DIR="$DIRECTORY_TO_ZIP/files/article/images"
@@ -38,16 +44,17 @@ else
   echo "Image directory does not exist: $IMAGE_DIR"
 fi
 
-# 进入txt目录进行批量化压缩（如果存在）
+# 批量化压缩txt目录（如果存在）
 TXT_DIR="$DIRECTORY_TO_ZIP/files/article/txt"
 if [ -d "$TXT_DIR" ]; then
-  cd "$TXT_DIR"
-  for i in *; do
+  for i in "$TXT_DIR"/*; do
     if [ -d "$i" ]; then
-      zip -r "$DIR_OUT/files/article/txt/${i}.zip" "$i"
+      zip -r "$DIR_OUT/files/article/txt/"$i".zip" "$i"
     fi
   done
-  cd - # 返回原始目录
 else
   echo "Txt directory does not exist: $TXT_DIR"
 fi
+
+# 脚本执行完毕
+echo "Script completed."
